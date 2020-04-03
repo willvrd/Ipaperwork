16ed4bed2f78e16cf26c7ebace5099f636596cbb
William Verde <williamverde10@gmail.com>
2020-04-03 15:37:19 -0400

    Added: email pre footer

--- Resources/views/emails/UserPaperworkHistory.blade.php
+++ Resources/views/emails/UserPaperworkHistory.blade.php
@@ -7,38 +7,28 @@
 
   @includeFirst(['ipaperwork.emails.style', 'ipaperwork::emails.base.style'])
   
-
 </head>
 
 <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-        <table border="0" width="100%" cellpadding="0" cellspacing="0">
-        
-            <tbody>
+    <table border="0" width="100%" cellpadding="0" cellspacing="0">
+    <tbody>
             
-            <tr>
-                <td height="30"></td>
-            </tr>
+        <tr><td height="30"></td></tr>
             
-            <tr bgcolor="#4c4e4e">
-                <td width="100%" align="center" valign="top" bgcolor="#4c4e4e">
+        <tr bgcolor="#4c4e4e">
+            <td width="100%" align="center" valign="top" bgcolor="#4c4e4e">
         
                    
-                    @includeFirst(['ipaperwork.emails.header', 'ipaperwork::emails.base.header'])
+                @includeFirst(['ipaperwork.emails.header', 'ipaperwork::emails.base.header'])
                                       
-                    <!----------   main content------------>
-                    <table width="600" border="0" cellpadding="0" cellspacing="0" align="center" class="container" bgcolor="F2F2F2">
+                <!----------   main content------------>
+                <table width="600" border="0" cellpadding="0" cellspacing="0" align="center" class="container" bgcolor="F2F2F2">
+                    <tbody>
+    
+                    <tr bgcolor="F2F2F2"><td height="20"></td></tr>
+                    <!---------- end header --------->
         
-        
-                        <!--------- Header  ---------->
-                        <tbody>
-        
-                        <tr bgcolor="F2F2F2">
-                            <td height="20"></td>
-                        </tr>
-                        <!---------- end header --------->
-        
-        
-                        <!--------- main section --------->
+                    <!--------- main section --------->
                         <tr>
                             <td>
                                 <table border="0" width="560" align="center" cellpadding="0" cellspacing="0"
@@ -80,40 +70,26 @@
                                     </tbody>
                                 </table>
                             </td>
-                        </tr><!--------- end main section --------->
-    
-                        <tr>
-                            <td align="center" mc:edit="copy2"
-                                style="color: #939393; font-size: 11px; font-weight: normal; font-family: Helvetica, Arial, sans-serif;"
-                                class="prefooter-subheader">
-                                <multiline>
-                                    <span style="color: #000">Dirección</span> 
-                                    Carrera 5 sur #83-40 local 14 Mall florida 3, mezzanine. Ibagué -Tolima. &nbsp;&nbsp;&nbsp;
-                                    <span style="color: #000">Tlf :</span> 312 676 1567 &nbsp;&nbsp;&nbsp;
-                                    <span style="color: #000">Email :</span> info@asesoriashc.com 
-                                </multiline>
-                            </td>
                         </tr>
+                    <!--------- end main section --------->
+    
+                    @includeFirst(['ipaperwork.emails.pre-footer', 'ipaperwork::emails.base.pre-footer'])
         
-                        <tr><td height="30"></td></tr>
+                    <tr><td height="30"></td></tr>
 
-                        </tbody>
-                    </table>
-                    <!------------ end main Content ----------------->
+                    </tbody>
+                </table>
+                <!------------ end main Content ----------------->
                    
-                    @includeFirst(['ipaperwork.emails.footer', 'ipaperwork::emails.base.footer'])
+                @includeFirst(['ipaperwork.emails.footer', 'ipaperwork::emails.base.footer'])
                     
-                </td>
-            </tr>
+            </td>
+        </tr>
         
-            <tr>
-                <td height="30"></td>
-            </tr>
+        <tr><td height="30"></td></tr>
         
-            </tbody>
-            
-            
-        </table>
+    </tbody>        
+    </table>
 </body>
 
 </html>
---
+++ Resources/views/emails/base/pre-footer.blade.php
@@ -0,0 +1,12 @@
+<tr>
+    <td align="center" mc:edit="copy2"
+        style="color: #939393; font-size: 11px; font-weight: normal; font-family: Helvetica, Arial, sans-serif;"
+        class="prefooter-subheader">
+        <multiline>
+            <span style="color: #000">Dirección</span> 
+            Carrera 5 sur #83-40 local 14 Mall florida 3, mezzanine. Ibagué -Tolima. &nbsp;&nbsp;&nbsp;
+            <span style="color: #000">Tlf :</span> 312 676 1567 &nbsp;&nbsp;&nbsp;
+            <span style="color: #000">Email :</span> info@asesoriashc.com 
+        </multiline>
+    </td>
+</tr>
